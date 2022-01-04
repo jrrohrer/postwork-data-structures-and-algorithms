c0924e3353eaf47e549130728a0581c26bfce598
jrrohrer <jessie.rohrer@gmail.com>
2022-01-04 11:19:59 -0500

    solved

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,9 @@
 def fibonacci(num)
-  # type your code in here
+  if num < 2
+    num
+  else
+    fibonacci(num - 1) + fibonacci(num - 2)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,7 +15,8 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 1"
   puts "=>", fibonacci(2)
 
-  puts
+  puts "Expecting: 8"
+  puts "=>", fibonacci(6)
 
   puts "Expecting: 55"
   puts "=>", fibonacci(10)
@@ -20,4 +25,7 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+  # if num is less than 2, return num
+  # else recursively return num - 1 + num - 2
 # And a written explanation of your solution
+  # I am using recursion to handle returning the correct value based on the number passed in, rather than storing a value and working forward from there.
