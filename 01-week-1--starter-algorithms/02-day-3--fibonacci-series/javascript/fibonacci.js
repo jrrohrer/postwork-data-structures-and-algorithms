60f814a3bad0432f59c577a54ecfdc8892763aba
jrrohrer <jessie.rohrer@gmail.com>
2022-01-04 11:15:26 -0500

    solved

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
-  // type your code here
+  if (num < 2) {return num}
+  let fib = [0, 1]
+  for (i = 0; i < num -1; ++i) {
+    let total = fib[0] + fib[1]
+    fib = [fib[1], total]
+  }
+  return fib[1]
 }
 
 if (require.main === module) {
@@ -12,7 +18,8 @@ if (require.main === module) {
   console.log("Expecting: 1");
   console.log("=>", fibonacci(2));
 
-  console.log("");
+  console.log("Expecting: 8");
+  console.log("=>", fibonacci(6))
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
@@ -21,4 +28,9 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+  // if num is less than 2, return num (this accounts for num being 0 or 1)
+  // initialize fib array with 0 and 1
+  // create a for loop that iterates up to the value of num
+  // create a total variable that stores the values of the fib array added together
+  // reset the fib array with new values: the 2nd number from the previous version, and the value of total
+  // return the second value from the fib array
