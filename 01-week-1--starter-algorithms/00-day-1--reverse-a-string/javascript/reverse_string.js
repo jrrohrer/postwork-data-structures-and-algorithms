bddfc5e3cb3c8cb1c3ff27f7e1fb642cf61e8dac
jrrohrer <jessie.rohrer@gmail.com>
2022-01-04 10:45:04 -0500

    solved

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let newStr = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+  return newStr
 }
 
 if (require.main === module) {
@@ -7,7 +11,8 @@ if (require.main === module) {
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
-  console.log("");
+  console.log("Expecting: 'eissej'");
+  console.log("=>", reverseString('jessie'))
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
@@ -16,4 +21,12 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+  // initialize an empty string called 'newStr'
+  // iterate over each character in the string from back to front
+    // push the last character in the string onto the newStr
+  // return the newStr
+
 // And a written explanation of your solution
+  // The function accepts a string as an argument. A variable called newStr is initialized and set to an empty string. 
+  // A for loop is used to iterate over the array, starting at the end, until there are no more characters in the array. 
+  // The characters are added to the empty newStr in reverse order using the push method. Then newStr is returned.
