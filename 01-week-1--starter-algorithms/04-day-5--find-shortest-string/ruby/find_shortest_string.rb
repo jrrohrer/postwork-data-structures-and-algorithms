a7716e475cc195eb87c21635cfb74be1c036691f
jrrohrer <jessie.rohrer@gmail.com>
2022-01-06 16:23:14 -0500

    solved

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
@@ -1,5 +1,6 @@
 def find_shortest_string(arr)
-  # type your code in here
+  sorted_arr = arr.sort_by(&:length)
+  sorted_arr[0]
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -19,7 +20,17 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 
   # BENCHMARK HERE
+  start_time = Time.now
+
+  1000.times do
+    find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
+  end
+
+  avg_time = (Time.now - start_time) / 1000.0
+  puts avg_time
 end
 
 # Please add your pseudocode to this file
+  # sort the given array in ascending order
+  # return the value at the first position in the array
 # And a written explanation of your solution
