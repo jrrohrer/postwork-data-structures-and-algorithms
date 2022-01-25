82c7b09e6f0d0806debf637f4f71e4fa7e1007a2
jrrohrer <jessie.rohrer@gmail.com>
2022-01-25 13:42:22 -0500

    solved

--- 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
+++ 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
@@ -1,18 +1,24 @@
 def recursive_search(arr, target)
-  # type your code in here
+  return true if arr.first == target
+  return false if !arr.include?(target)
+  
+  recursive_search(arr[1..-1], target)
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: true"
   puts "=>", recursive_search([1, 2, 3], 2)
 
-  puts
+  puts "Expecting: true"
+  puts "=>", recursive_search([22, 23, 24, 25, 26], 24)
 
   puts "Expecting: false"
   puts "=>", recursive_search([3, 2, 1], 4)
 
-  # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
+  # return true if the target is the first element in the array
+  # return false if the array does not include the target
+  # recursively call the method to iterate over the rest of the array
 # And a written explanation of your solution
